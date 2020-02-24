cfa8a53d8c6fe78106a8bc66ba9ae4e51623e4d5
Steve Ryan <syntithenai@gmail.com>
2020-02-24 12:15:02 +1100

    deepspeech update to 0.6.1

--- deepspeech-model/install.sh
+++ deepspeech-model/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ ! -f 'models/lm.binary' ]; then
-	wget  -qO- -c https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-models.tar.gz | tar xvz 
+	wget  -qO- -c https://github.com/mozilla/DeepSpeech/releases/download/v0.6.1/deepspeech-0.6.1-models.tar.gz | tar xvz 
 fi
