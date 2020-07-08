15b429e8a9c8f2057ba08c138e27f74284c9158b
Steve Ryan <syntithenai@gmail.com>
2020-07-08 16:45:53 +1000

    wip

---
+++ hermod-python/www/spokencrossword/build/precache-manifest.5d0a789479758a37264c96c4a1c19e75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94db6deec465cb67681257062c071ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ddb57bca45ead357596",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9424216d09ffb12aaf50",
+    "url": "/static/css/main.1abba7a6.chunk.css"
+  },
+  {
+    "revision": "1ddb57bca45ead357596",
+    "url": "/static/js/2.7c77ea25.chunk.js"
+  },
+  {
+    "revision": "df65b9ff1d5957f44d4bd7d8dcd36b6b",
+    "url": "/static/js/2.7c77ea25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9424216d09ffb12aaf50",
+    "url": "/static/js/main.5e70b5a4.chunk.js"
+  },
+  {
+    "revision": "2c7d113fb83e004068f8",
+    "url": "/static/js/runtime-main.3269aaef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d362cb3f61deb313ac87505e59641c7b",
+    "url": "/static/media/waiting_small.d362cb3f.gif"
+  }
+]);
